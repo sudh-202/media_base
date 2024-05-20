c70a43711e4997f7670e62d061f7c13ab0f3c613
sudh202 <srivastavasudhanshu202@gmai.com>
2024-05-20 19:31:47 +0530

    test changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,7 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14 pt-20 pb-6">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           
+          {/* Changes */}
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
             {FOOTER_LINKS.map((columns) => (
