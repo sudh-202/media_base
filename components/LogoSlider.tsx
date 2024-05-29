5baa1fbd3bada6a9ddf95b3613f0815a61377033
sudh202 <srivastavasudhanshu202@gmai.com>
2024-05-29 16:19:36 +0530

    LogoSlider update

--- components/LogoSlider.tsx
+++ components/LogoSlider.tsx
@@ -142,6 +142,13 @@ import Image from 'next/image';
           <React.Fragment key={card.id}>
             <div className="   w-24 h-28 bg-gray-100 rounded-lg flex flex-col items-center justify-center m-2">
               <img src={card.icon} alt={card.title} className="w-14 h-14 mb-2 object-contain" />
+              <Image  
+                    src={card.icon}
+                     alt={card.title}
+                     className="w-14 h-14 mb-2 object-contain"
+                      width={136}
+                      height={96}
+                  />
               <p className="text-center">{card.title}</p>
             </div>
             {((index + 1) % 3 === 0) && <div className="w-full"></div>}
